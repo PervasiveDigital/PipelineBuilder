c8e6415b7e5d1af538cd7b9095075990dfd99746
Martin Calsyn <martin@pervasive.digital>
2015-07-27 17:18:16 +0300

    moving to vsix

--- PipelineBuilder/PipelineBuilder/VSPipelineBuilder/IPipelineBuilderWorker.cs
+++
@@ -1,9 +0,0 @@
-﻿/// Copyright (c) Microsoft Corporation.  All rights reserved.
-using System;
-namespace VSPipelineBuilder
-{
-    interface IPipelineBuilderWorker
-    {
-        System.Collections.Generic.List<PipelineBuilder.PipelineSegmentSource> BuildPipeline(string sourceFile);
-    }
-}
